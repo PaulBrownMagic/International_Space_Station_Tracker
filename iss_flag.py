545e51502e6bd989164fd8fe91d2da9c29a06a90
Paul Brown <pbrownpaul@gmail.com>
2016-04-06 23:43:09 +0100

    Update iss_flag.py

--- iss_flag.py
+++ iss_flag.py
@@ -5,7 +5,7 @@ from pygame import *
 import sys
 # Constants
 SCREEN_WIDTH = 260
-SCREEN_HEIGHT = 260
+SCREEN_HEIGHT = 220
 FPS = 1 / 10  # 1 frame every 10 seconds
 ISS_URL = 'http://api.open-notify.org/iss-now.json'
 # Setup PyGame and geolocator
@@ -50,7 +50,7 @@ while not done:
         longitude, latitude = get_long_lat()
         country_code, country = get_location(longitude, latitude)
         if file_name != "country-flags/png250px/" + country_code + ".png":
-            screen.fill((0,0,0))
+            screen.fill((35,35,45))
             file_name = "country-flags/png250px/" + country_code + ".png"
             flag = image.load(file_name)
             x_pos = (SCREEN_WIDTH - flag.get_rect().size[0])//2
@@ -58,6 +58,7 @@ while not done:
             screen.blit(flag, (x_pos, y_pos))
             display.set_caption("ISS - " + country )
     except:
+        file_name = None
         print("Error")
         display.set_caption("ISS")
     print()
